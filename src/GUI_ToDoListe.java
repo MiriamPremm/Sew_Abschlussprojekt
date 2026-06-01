bc6b978e0a9c4950921202f1394d9993eadb5a17
Jakob <4191@htl.rennweg.at>
2026-06-01 11:15:17 +0200

    FX gemacht und begonnen

---
+++ src/GUI_ToDoListe.java
@@ -0,0 +1,17 @@
+import javafx.application.Application;
+import javafx.scene.control.CheckBox;
+import javafx.stage.Stage;
+
+public class GUI_ToDoListe extends Application {
+
+    CheckBox checkBox = new CheckBox("Text");
+
+    @Override
+    public void start(Stage stage) {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
