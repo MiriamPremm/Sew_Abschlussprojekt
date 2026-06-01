67b8a5509540e571369727ce446fcace81f34365
Jakob <4191@htl.rennweg.at>
2026-06-01 11:34:46 +0200

    FX gemacht und begonnen

--- src/GUI_ToDoListe.java
+++ src/GUI_ToDoListe.java
@@ -1,14 +1,21 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class GUI_ToDoListe extends Application {
 
-    CheckBox checkBox = new CheckBox("Text");
-
     @Override
     public void start(Stage stage) {
+        stage.setTitle("HBox in BorderPane");
 
+        BorderPane borderPane = new BorderPane();
+        Scene scene = new Scene(borderPane, 400, 200); // w, h
+        stage.setScene(scene);
+
+
+        stage.show();
     }
 
     public static void main(String[] args) {
