a2df9a5620bb021813e45b18f68b059218428a1b
Jakob <4191@htl.rennweg.at>
2026-06-07 22:19:13 +0200

    FX gemacht und begonnen

--- src/GUI_ToDoListe.java
+++ src/GUI_ToDoListe.java
@@ -1,19 +1,33 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class GUI_ToDoListe extends Application {
-
     @Override
     public void start(Stage stage) {
+
+
+        CheckBox cd = new CheckBox("Mathe lernen");
+
+
+
+
+
+
+        /*VBox vbox1 = new VBox(10);
         stage.setTitle("HBox in BorderPane");
+
+        TextField aufgabeFeld = new TextField();
+        aufgabeFeld.setPromptText("Neue Aufgabe eingeben");    */
 
         BorderPane borderPane = new BorderPane();
         Scene scene = new Scene(borderPane, 400, 200); // w, h
         stage.setScene(scene);
-
+        borderPane.setLeft(cd);
 
         stage.show();
     }
